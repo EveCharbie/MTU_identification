f7174617de746a49fef376c049dd7bb6323541f6
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2023-10-27 13:55:49 +0400

    muscle equation visualisation

--- code/test.m
+++ code/test.m
@@ -1,5 +1,6 @@
-% test casadi functions
+%% test casadi functions
 %run('.\De_Groote'); 
+%% test du model géometrique (OK)
 De_Groote ; 
 % foot; leg; thigh
 segment_length_num = [.2; .4; .45];
@@ -23,4 +24,83 @@ umt_length = UMTLength(q_num, known_parameters_num);
 moment_arms = MomentArm(q_num, known_parameters_num);
 
 
-plotmodel(muscle_origin_in0, muscle_insertion_in0, joint_centers)
+% plotmodel(muscle_origin_in0, muscle_insertion_in0, joint_centers)
+
+
+%% test du model réologique
+
+lm_num = [0 : 0.01 : 1.7 ; 0 : 0.01 : 1.7 ; 0 : 0.01 : 1.7] ; 
+lt_num =  [linspace(0.95,1.05,length(lm_num(1,:))) ; linspace(0.95,1.05,length(lm_num(1,:))) ; linspace(0.95,1.05,length(lm_num(1,:)))] ; 
+a_num = ones(3,1) ; 
+% TA; SO; GA;
+l0m_num = [0.098 ;  0.031 ; 0.09] ; 
+phi0_num = [0.29670597 ; 0.20943951 ; 0.29670597 ] ;
+f0m_num = [3000 ; 3600 ; 2500] ; 
+lst_num = [0.08726646 ; 0.31 ; 0.36 ] ; 
+muscle_tendon_parameters_num =  [l0m_num ; phi0_num ; f0m_num ; lst_num ] ; 
+
+% from OISM
+
+% Sol_f0m = 3600 ;
+% Sol_l0m = 0.031 ;
+% Sol_phi0 = 0.20943951 ; %rad
+% Sol_lst = 0.31 ;
+% 
+% % Gast
+% Gast_f0m = 2500 ;
+% Gast_l0m = 0.09 ;
+% Gast_phi0 = 0.29670597 ; %rad
+% Gast_lst = 0.36 ;
+% 
+% % Ta
+% Ta_f0m = 3000 ;
+% Ta_l0m = 0.098 ;
+% Ta_phi0 = 0.29670597 ; %rad
+% Ta_lst = 0.08726646 ;
+
+%%
+for i = 1 :  length (lm_num)
+    % passives muscle force (Normalized)
+    PMFN = Normalized_Muscle_Passive_Force_Length(q_num,known_parameters_num,lm_num(:,i)) ; 
+    NormalizedMusclePassiveForceLength_num(:,i) = full(PMFN) ; 
+    % active muscle force (Normalized)
+    AMFN = Normalized_Muscle_Active_Force_Length(q_num,known_parameters_num,lm_num(:,i)) ; 
+    NormalizedMuscleActiveForceLength_num(:,i) = full(AMFN) ; 
+    % total muscle force (Normalized)
+    MFN = Normalized_Muscle_Force(q_num,known_parameters_num,lm_num(:,i),a_num(:)) ; 
+    NormalizedMuscleForce_num(:,i) = full(MFN) ; 
+    % total tendon force (Normalized)
+    TFN = Normalized_Tendon_Force(q_num,known_parameters_num,lt_num(:,i)) ; 
+    NormalizedTendonForce_num(:,i) = full(TFN) ; 
+
+end 
+
+%% illustration
+
+figure("Name","relation force longueur ","Color",[1 1 1])
+subplot(1,2,1)
+plot(lm_num(1,:) , NormalizedMusclePassiveForceLength_num(1,:), 'k')
+hold on 
+plot(lm_num(1,:) , NormalizedMuscleActiveForceLength_num(1,:), 'r')
+plot(lm_num(1,:) , NormalizedMuscleForce_num(1,:), 'k.')
+xlabel('Normalized length','FontWeight','bold')
+ylabel('Normalized','FontWeight','bold')
+xlim([0.2 1.7])
+ylim([0 1.4])
+title('Muscle','FontAngle','italic')
+legend(["Passive", "Active","Total"])
+hold off
+
+subplot(1,2,2)
+plot(lt_num(1,:) , NormalizedTendonForce_num(1,:), 'k')
+hold on 
+xlabel('Normalized length','FontWeight','bold')
+ylabel('Normalized force','FontWeight','bold')
+xlim([0.95 1.05])
+ylim([0 1])
+
+title('Tendon','FontAngle','italic')
+hold off
+
+%% 
+
