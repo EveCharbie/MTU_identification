28744836cf55e82cb71a7b002331a44832bdd4ac
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2023-11-05 00:00:51 +0400

    Test forces
    Put the tendon length and muscle length in equa
    --> Values not OK

--- code/Root_finder_test.m
+++ code/Root_finder_test.m
@@ -1,8 +1,8 @@
-    %% test of the root finder 
+    %% test of the root finder [value NOT OK]
 Model_Opensim ; 
 
 known_parameters_num = [segment_geometry_num; muscle_origin; muscle_insersion];
-q_num = [0, 0 , 0, 0, (10/180)*pi , (0/180)*pi ] ; 
+q_num = [0, 0 , 0, 0, (0/180)*pi , (0/180)*pi ] ; 
 % geometry 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [muscle_origin_in0, muscle_insertion_in0, joint_centers] = ForwardKinematics(q_num, known_parameters_num);
@@ -13,7 +13,7 @@ plotmodel(muscle_origin_in0, muscle_insertion_in0, joint_centers)
     
 % neuro musculo skeltical states (random)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-a_num = [.2, .3, .4]; 
+a_num = [.0, .0, .0]; 
   
 
 % muscle tendon parameter
@@ -25,4 +25,16 @@ p_num = horzcat(a_num,musculoskeletal_states_num , muscle_tendon_parameters_num)
 
 
 x0 = [0.001, 0.001, 0.001 ,l0m_num] ;  % tendonLengthening, fibre length   (TA SOL GAST)
-equilibrateMuscleTendon(x0, p_num)
+x_num = equilibrateMuscleTendon(x0, p_num) ;
+
+tendonLengthening_num = x_num(1:nMuscles);
+fiberLength_num = x_num(nMuscles+1:end);
+
+all_states_num = [musculoskeletal_states_num,  tendonLengthening_num', fiberLength_num' , a_num] ;
+
+pennationAngle_ = getPennationAngle(all_states_num,muscle_tendon_parameters_num) ; % Not OK [AM]
+tendonForce_ = getTendonForce(all_states_num, muscle_tendon_parameters_num)
+muscleForce_ = getMuscleForce(all_states_num, muscle_tendon_parameters_num)
+muscleAForce_ = getMuscleActiveForce(all_states_num, muscle_tendon_parameters_num);
+
+
