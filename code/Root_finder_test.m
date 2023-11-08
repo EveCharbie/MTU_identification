83975299a2f7adab89d720a355a1d661e65c0e62
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2023-11-08 11:14:14 +0400

    import musculoskeletical parameter with fun  Opensim_extraction

--- code/Root_finder_test.m
+++ code/Root_finder_test.m
@@ -1,43 +1,44 @@
     %% test of the root finder [value NOT OK]
-Model_Opensim ; 
+%Model_Opensim ; 
+De_Groote_opensim ;
+%
+[known_parameters_num,muscle_tendon_parameters_num] = Opensim_extraction()
 
-known_parameters_num = [segment_geometry_num; muscle_origin; muscle_insersion];
+%known_parameters_num = [segment_geometry_num; muscle_origin; muscle_insersion];
 
 
 % input :  muscle activation and joint orientation  (random)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-a_num = [.01, .01, .01]; 
-q_num = [0, 0 , 0, 0, (0/180)*pi , (-30/180)*pi ] ; 
+a_num = [.3, .2, .2]; 
+q_num = [0, 0 , 0, 0, (10/180)*pi , (-30/180)*pi ] ; 
 
 % geometry 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[muscle_origin_in0, muscle_insertion_in0, joint_centers] = ForwardKinematics(q_num, known_parameters_num);
+[muscle_origin_in0, muscle_insertion_in0,muscle_viaPoint_in0, joint_centers] = ForwardKinematics(q_num, known_parameters_num);
 umt_length = getUMTLength(q_num, known_parameters_num) ; 
 moment_arms = getMomentArm(q_num, known_parameters_num) ; 
 
 %plotmodel(muscle_origin_in0, muscle_insertion_in0, joint_centers)
     
 
-  
-
 % muscle tendon parameter
-muscle_tendon_parameters_num =  [l0m_num , phi0_num , f0m_num , lst_num ] ; 
+%muscle_tendon_parameters_num =  [l0m_num , phi0_num , f0m_num , lst_num ] ; 
     % musculoskeletal states (q, bones length , muscletendon insertion) 
-musculoskeletal_states_num = [q_num , known_parameters_num'] ; 
+musculoskeletal_states_num = [q_num , known_parameters_num] ; 
 
 p_num = horzcat(a_num,musculoskeletal_states_num , muscle_tendon_parameters_num) ;
 
 
-x0 = [0.001, 0.001, 0.001 ,l0m_num] ;  % tendonLengthening, fibre length   (TA SOL GAST)
+x0 = [0.001, 0.001, 0.001 ,muscle_tendon_parameters_num(1:3)] ;  % tendonLengthening, fibre length   (TA SOL GAST)
 x_num = equilibrateMuscleTendon(x0, p_num) ;
 
-tendonLengthening_num = x_num(1:nMuscles);
-fiberLength_num = x_num(nMuscles+1:end);
+tendonLengthening_num = x_num(1:nMuscles)';
+fiberLength_num = x_num(nMuscles+1:end)';
 
-all_states_num = [musculoskeletal_states_num,  fiberLength_num', tendonLengthening_num' , a_num] ;
+all_states_num = [musculoskeletal_states_num,  fiberLength_num, tendonLengthening_num , a_num] ;
 
 pennationAngle_ = getPennationAngle(all_states_num,muscle_tendon_parameters_num) ; % Not OK [AM]
 tendonForce_ = getTendonForce(all_states_num, muscle_tendon_parameters_num) ;
 muscleForce_ = getMuscleForce(all_states_num, muscle_tendon_parameters_num) ; 
-muscleAForce_ = getMuscleActiveForce(all_states_num, muscle_tendon_parameters_num)
-musclePForce_ = getMusclePassiveForce(all_states_num, muscle_tendon_parameters_num)
+muscleAForce_ = getMuscleActiveForce(all_states_num, muscle_tendon_parameters_num) ;
+musclePForce_ = getMusclePassiveForce(all_states_num, muscle_tendon_parameters_num) ;
