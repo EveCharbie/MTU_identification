acd7e7999cccfb631adec1cce697e08700a87233
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2023-11-15 11:08:50 +0400

    test of the function

--- code/Root_finder_test.m
+++ code/Root_finder_test.m
@@ -1,25 +1,24 @@
     %% test of the root finder [value NOT OK]
 %Model_Opensim ; 
 De_Groote_opensim ;
-%
-[known_parameters_num,muscle_tendon_parameters_num] = Opensim_extraction()
 
-%known_parameters_num = [segment_geometry_num; muscle_origin; muscle_insersion];
-
+[known_parameters_num,muscle_tendon_parameters_num] = Opensim_extraction() ;
 
 % input :  muscle activation and joint orientation  (random)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-a_num = [.3, .2, .2]; 
-q_num = [0, 0 , 0, 0, (10/180)*pi , (-30/180)*pi ] ; 
+a_num = [1, 1, 1]; 
+q_num = [0, 0 , 0, 0, (0/180)*pi , (30/180)*pi ] ; 
 
 % geometry 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [muscle_origin_in0, muscle_insertion_in0,muscle_viaPoint_in0, joint_centers] = ForwardKinematics(q_num, known_parameters_num);
-umt_length = getUMTLength(q_num, known_parameters_num) ; 
+umt_length = getUMTLength(q_num, known_parameters_num) ;
 moment_arms = getMomentArm(q_num, known_parameters_num) ; 
 
 %plotmodel(muscle_origin_in0, muscle_insertion_in0, joint_centers)
     
+
+
 
 % muscle tendon parameter
 %muscle_tendon_parameters_num =  [l0m_num , phi0_num , f0m_num , lst_num ] ; 
@@ -35,10 +34,9 @@ x_num = equilibrateMuscleTendon(x0, p_num) ;
 tendonLengthening_num = x_num(1:nMuscles)';
 fiberLength_num = x_num(nMuscles+1:end)';
 
+
 all_states_num = [musculoskeletal_states_num,  fiberLength_num, tendonLengthening_num , a_num] ;
 
-pennationAngle_ = getPennationAngle(all_states_num,muscle_tendon_parameters_num) ; % Not OK [AM]
-tendonForce_ = getTendonForce(all_states_num, muscle_tendon_parameters_num) ;
-muscleForce_ = getMuscleForce(all_states_num, muscle_tendon_parameters_num) ; 
-muscleAForce_ = getMuscleActiveForce(all_states_num, muscle_tendon_parameters_num) ;
-musclePForce_ = getMusclePassiveForce(all_states_num, muscle_tendon_parameters_num) ;
+
+Force_length_representation(all_states_num,muscle_tendon_parameters_num,casadiFun,NameMuscles)
+
