124499183bbc7c750fa58151642acb0a9f2774c1
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2023-11-16 15:33:18 +0400

    nlp don't work yet --> L122

--- code/NLP_identification_simul.m
+++ code/NLP_identification_simul.m
@@ -1,8 +1,17 @@
+% import casadi
+[ret, name] = system('hostname');
+if strcmp(name, '942-27984')
+    addpath('C:\Users\Stage\Desktop\Neuromusculoskeletal Modeling\Casadi')
+elseif strcmp(name, 'xxx')
+    addpath('/Users/mickaelbegon/Downloads/casadi-3.6.3-osx64-matlab2018b/')
+end
+addpath('C:\Users\Stage\Desktop\Neuromusculoskeletal Modeling\Casadi')
+
+import casadi.*
+
 
 % Call data_generator
-
-
-
+hypothetical_data_generator ;
 
 % Add noise
 
@@ -13,98 +22,112 @@ w0 = []; % initial guess
 lbw = []; % lower bound of variable
 ubw = []; % upper bound of variable
 J = 0; % cost | objective function
-g={}; % constraints
+gg={}; % constraints
 lbg = []; % lower bound of constraints
 ubg = []; % upper bound of constraints
 
+UP = horzcat(unknown_parameters(:,1)',unknown_parameters(:,2)',...
+    unknown_parameters(:,3)',unknown_parameters(:,4)') ;
 
 % create muscle parameters variables
-w = { w{:}, unknown_parameters};
+w = { w{:}, UP};
 
 % we should find muscle_tendon_parameters_num
-w0 =  [w0; muscle_tendon_parameters_num + rand()*xxx]; %todo
+% w0 =  [w0; muscle_tendon_parameters_num ]; %todo
 
-lbw = [lbw; muscle_tendon_parameters_num * 0.5];%todo
-ubw = [ubw; muscle_tendon_parameters_num * 2];%todo
-
+% w0 =  [w0; muscle_tendon_parameters_num + rand()*xxx]; %todo
 
 
-% header = {'Torque','q1','q2',... 1..3
+
+% header = header = {'Torque','q1','q2',...
 %     'activation_tibialis','activation_soleus','activation_gastrocnemius',...
-%     'momentarm_tibialis','momentarm_soleus','momentarm_gastrocnemius',...
-%     'UMT_tibialis','UMT_soleus','UMT_gastrocnemius',... todo: REMOVE
 %     'fiber_tibialis','fiber_soleus','fiber_gastrocnemius',...
-%     'phi_tibialis','phi_soleus','phi_gastrocnemius',...
-%      'tendon_tibialis','tendon_soleus','tendon_gastrocnemius'} ;
+%     'phi_tibialis','phi_soleus','phi_gastrocnemius'} ;
 
 
 % create variable for each trial
 
 % Weightings in cost function
-W_torque = 1;
-W_length = xx; %todo 1Nm correspond à 1 mm?
-W_angle = xx;  % 1 Nm correspond à 3 deg?
-
-for trial = 1:size(results, 1)
-    fiberLength_k = MX.sym(['Fiber_length_' num2str(trial)], nMuscles);
-    tendonLengthening_k = MX.sym(['Tendon_Lengthening_' num2str(trial)], nMuscles);
-
-    w = { w{:}, fiberLength_k, tendonLengthening_k};
-    w0 =  [w0; xxxx]; %todo add initial guess
-    lbw = [lbw; ones(3,1)*0.01; zeros(3,1)];%todo
-    ubw = [ubw; ones(3,1)*0.4; ones(3,1)*.2];%todo
+W_torque = 1; % 1 Nm
+W_length = 0.005; %todo 1Nm correspond à 50 mm
+W_angle = (3/180) * pi;  % 1 Nm correspond à 3 deg
 
 
-    data = trial{trial};
-
-    % all_states = vertcat(musculoskeletal_states, muscle_tendon_states);
-    % musculoskeletal_states = vertcat(q, known_parameters)
-    % muscle_tendon_states = vertcat(fiberLength, tendonLengthening, a);
-
-    q = data(2:3); 
-
-    all_states_num = [q; known_parameters; musculoskeletal_states; muscle_tendon_states]; %todo
-    
-    
 
 
-    
-    
-    umt_lengths = getUMTLength(q, known_parameters);
-    torque = getJointMoment(xxx); %todo add input
-    tendonL = 
-    
+
+for trial = 1 % : ntrials % for 1 to nb trials
+
+    %  the bounds
+    w = { w{:}, UP};
+    w0 =  [w0; muscle_tendon_parameters_num ]; 
+    lbw = [lbw; muscle_tendon_parameters_num * 0.5]; % lower bound of variable
+    ubw = [ubw; muscle_tendon_parameters_num * 2]; % upper bound of variable
+    lbg = [lbg; zeros(1,6)]; % lower bound of constraints
+    ubg = [ubg; [0.05, 0.05, 0.05 ,muscle_tendon_parameters_num(1:3)*1.8]]; % upper bound of constraints
+
+    fiberLength_k = SX.sym(['Fiber_length_' num2str(trial)], nMuscles);
+    tendonLengthening_k = SX.sym(['Tendon_Lengthening_' num2str(trial)], nMuscles);
+    x = vertcat((tendonLengthening_k),(fiberLength_k)) ; 
+
+    data = Data(trial,:);
+
+        % known variables 
+    a_trial = data(4:6) ; % muscle activation during the trial
+    q_trial = [0, 0, 0, 0, data(2:3)] ; % skeleton configuration during the trial
+    musculoskeletal_states_trial = [q_trial, known_parameters_num ] ; % muscleskeleton configuration during the trial
+    neuromusculoskeletal_states_trial = [a_trial, musculoskeletal_states_trial] ; % Neuromusculoskeletal states
+
+
+    %x0 = [0.001, 0.001, 0.001, w0(trial,1:3)] ;  % tendonLengthening, fibre length   (TA SOL GAST)
+    p_trial = horzcat(neuromusculoskeletal_states_trial, w{trial}) ;
+
+    umtLength = casadiFun.getUMTLength(musculoskeletal_states_trial) ; 
     % constraints
-    contraints = g(x,p); %todo rename g in de_groot
-    g={g, constraints}; 
-    lbg = [lbg; zeros(6,1)]; 
-    ubg = [ubg; zeros(6,1)]; 
+
+    [g0,g1]  =  muscletendonequation(a_trial,fiberLength_k,tendonLengthening_k,unknown_parameters,umtLength) ; 
+
+    g = Function('g', {x, p}, {vertcat(g0,g1)},{'x', 'p'}, {'residuals'}) ;
+
+
+    contraints = g(x,p_trial);
+
+    tendonLengthening_trial = contraints(1:nMuscles)' ; 
+    fiberLength_trial = contraints(nMuscles+1:end)' ;
+
+    rootedvariables_trial = [fiberLength_trial ,tendonLengthening_trial] ;
+    all_states_trial = [neuromusculoskeletal_states_trial, rootedvariables_trial] ;
+
+    temp = casadiFun.getJointMoment(all_states_trial,  w{trial}) ;
+    Torque_simulated = temp(end) ;
+    FiberLength_simulated = fiberLength_trial ;
+    phi_simulated = casadiFun.getPennationAngle(all_states_trial,w{trial})' ;
 
     % objective
+     J = J + W_torque * (data(1) - Torque_simulated)^2; %add error on joint torque
+     J = J + W_length * sum((data(7:9) - FiberLength_simulated).^2);% add error on tendon length and pennation angle
+     J = J + W_angle * sum((data(10:12) - phi_simulated).^2);
 
-
-    J = J + W_torque * (data(1) - torque)^2; %add error on joint torque
-    
-    % add error on tendon length and pennation angle
-    J = J + W_length * (data(xxx) - tendonLength).^2;
-    J = J + W_angle * (data(xxx) - pennationAngle).^2;
+     gg = { gg{:}, contraints};
 
 end
 
 
 
+% % Create an NLP solver
+%prob = struct('f', J, 'x', w1, 'g',g1);
 
+% nlp prob : 
+% "x" opt parameters, 'f' function to minimized, 'g' contraint function 
+prob = struct('x', [vertcat(w{1}),vertcat(x')], 'f', J , 'g',vertcat(gg{:})); 
 
-
-% Create an NLP solver
-prob = struct('f', J, 'x', vertcat(w{:}), 'g', vertcat(g{:}));
 solver = nlpsol('solver', 'ipopt', prob);
 
+ 
 % Solve the NLP
 sol = solver('x0', w0, 'lbx', lbw, 'ubx', ubw,...
-            'lbg', lbg, 'ubg', ubg);
+    'lbg', lbg, 'ubg', ubg);
 w_opt = full(sol.x);
 
 param_opt = w_opt(1:nparam);
 
-%compare with muscle_tendon_parameters_num
