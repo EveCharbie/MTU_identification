6e6ac4bd4f7bf598b29438d0015f6ec46df953c0
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2024-02-02 14:52:00 +0400

    Use main to import Neuromusculoskeletal model and Run the NLP
    -  Import from Opensim Musculoskeletal Geometry and Generic Muscle Tendon Parameters from Opensim
    - Import Muscle Contraction Dynamics Equation (muscle tendon equation from De
    
    -Contains : Data generator and NLP
    % Groote) --> Hill type model --> Fmt = f(a, ℓmt, νmt; Fom, ℓom, ℓst, φo).

---
+++ code/main.m
@@ -0,0 +1,29 @@
+                        %% Main 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Set up 
+clear
+close all
+clc
+
+    %% Load Neuromusculoskeletal :
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Import from Opensim Musculoskeletal Geometry and Generic Muscle Tendon Parameters (ℓom, φo, Fom, ℓst)
+[known_parameters_num,muscle_tendon_parameters_num] = Model_OSIM2Mat() ; 
+
+% Import Muscle Contraction Dynamics (muscle tendon equation from De
+% Groote) --> Hill type model --> Fmt = f(a, ℓmt, νmt; Fom, ℓom, ℓst, φo).
+% Note that in our model we ignore :
+%       - fiber contraction velocity (νmt = 1)
+%       - and electromechanical delay (a(t) = e(t)) 
+[casadiFun,unknown_parameters,definition] = DeGrooteFunction() ; 
+
+    %% Data generator 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% generation of Hypothetical Data with the model 
+%[Data,header] = HypotheticalDataGenerator(known_parameters_num,muscle_tendon_parameters_num,casadiFun) ; 
+
+    %% NLP  NonLinear Programming
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Optimisation of Generic Muscle Tendon Parameters with experimental data 
+load('Data.mat')
+errParameters = NLP_identification_simulation(known_parameters_num,muscle_tendon_parameters_num,unknown_parameters,casadiFun,Data);
